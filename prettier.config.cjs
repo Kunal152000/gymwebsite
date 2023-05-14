f4d9de91c0d6941531bc963bb68708f47b16deac
kunal <kunal15febjha@gmail.com>
2023-05-15 02:46:12 +0530

    newchanges

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,1 +1,9 @@
-module.exports={plugins:[require("prettier-plugin-tailwindcss")],}
+// prettier.config.js
+// eslint-disable-next-line no-undef
+define(function (require, exports, module)
+{
+    module.exports = {
+        plugins: [require("prettier-plugin-tailwindcss")],
+      };
+
+})
