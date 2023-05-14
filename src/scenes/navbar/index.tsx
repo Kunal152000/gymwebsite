b6a6575a8fd1845093ff05b6b71f24bd699d5544
kunal <kunal15febjha@gmail.com>
2023-05-15 03:14:41 +0530

    toggle

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -83,11 +83,12 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
           </div>
 
           {/* MENU ITEMS */}
-          <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+          <div className="ml-[33%] flex flex-col gap-10 text-2xl" onClick={() => setIsMenuToggled(!isMenuToggled)}>
             <Link
               page="Home"
               selectedPage={selectedPage}
               setSelectedPage={setSelectedPage}
+              
             />
             <Link
               page="Benefits"
